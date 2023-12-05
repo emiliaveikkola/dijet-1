c71ac27e36ed8f2d244fc3b25099c5ef8d8c832f
emiliaveikkola <emiliaveikkola@icloud.com>
2023-12-05 16:00:40 +0200

    Added new bins

--- bins.py
+++ bins.py
@@ -1,10 +1,13 @@
 import numpy as np
 
 n_bins = 100
-bin_edges = np.logspace(0, 2, n_bins)
+bin_edges = np.logspace(0.1, 2, n_bins)
 
 # Offset the array to start from 0
 bin_edges -= bin_edges[0]
+
+# Round bin edges to 5 decimal places
+bin_edges = np.round(bin_edges, 5)
 
 # Print the generated bin edges
 print(f"n_bins = {n_bins}")
