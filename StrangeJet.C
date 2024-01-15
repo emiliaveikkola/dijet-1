ba10be51de0ee11ff03638d03354e456551abb59
emiliaveikkola <emiliaveikkola@icloud.com>
2024-01-15 15:20:42 +0200

    Corrections

--- StrangeJet.C
+++ StrangeJet.C
@@ -42,7 +42,17 @@ void StrangeJet::Loop()
 
 
   // Bins
-  Double_t xbins[] = {0.11633, 0.17847, 0.24341, 0.31129, 0.38224, 0.45639, 0.53389, 0.61489, 0.69955, 0.78804, 0.88053, 0.97719, 1.07823, 1.18382, 1.29419, 1.40955, 1.53011, 1.65613, 1.78784, 1.92549, 2.06937, 2.21975, 2.37693, 2.5412, 2.7129, 2.89235, 3.07992, 3.27595, 3.48085, 3.695, 3.91883, 4.15277, 4.39728, 4.65284, 4.91995, 5.19912, 5.49091, 5.79588, 6.11463, 6.44778, 6.79598, 7.15992, 7.5403, 7.93787, 8.3534, 8.7877, 9.24163, 9.71606, 10.21193, 10.73021, 11.2719, 11.83807, 12.42982, 13.0483, 13.69473, 14.37037, 15.07653, 15.8146, 16.58602, 17.39229, 18.23499, 19.11576, 20.03633, 20.99849, 22.00413, 23.0552, 24.15376, 25.30195, 26.50203, 27.75632, 29.06729, 30.43749, 31.86959, 33.36641, 34.93085, 36.56597, 38.27498, 40.0612, 41.92812, 43.8794, 45.91884, 48.05043, 50.27832, 52.60688, 55.04064, 57.58437, 60.24303, 63.02181, 65.92614, 68.96169, 72.1344, 75.45046, 78.91634, 82.53882, 86.32497, 90.28218, 94.41819, 98.74107}; // Adjust the bin edges as needed
+  Double_t xbins[] = {0.11633, 0.17847, 0.24341, 0.31129, 0.38224, 0.45639, 0.53389, 0.61489, 0.69955, 0.78804, 
+    0.88053, 0.97719, 1.07823, 1.18382, 1.29419, 1.40955, 1.53011, 1.65613, 1.78784, 1.92549, 
+    2.06937, 2.21975, 2.37693, 2.5412, 2.7129, 2.89235, 3.07992, 3.27595, 3.48085, 3.695, 
+    3.91883, 4.15277, 4.39728, 4.65284, 4.91995, 5.19912, 5.49091, 5.79588, 6.11463, 6.44778, 
+    6.79598, 7.15992, 7.5403, 7.93787, 8.3534, 8.7877, 9.24163, 9.71606, 10.21193, 10.73021, 
+    11.2719, 11.83807, 12.42982, 13.0483, 13.69473, 14.37037, 15.07653, 15.8146, 16.58602, 
+    17.39229, 18.23499, 19.11576, 20.03633, 20.99849, 22.00413, 23.0552, 24.15376, 25.30195, 
+    26.50203, 27.75632, 29.06729, 30.43749, 31.86959, 33.36641, 34.93085, 36.56597, 38.27498, 
+    40.0612, 41.92812, 43.8794, 45.91884, 48.05043, 50.27832, 52.60688, 55.04064, 57.58437, 
+    60.24303, 63.02181, 65.92614, 68.96169, 72.1344, 75.45046, 78.91634, 82.53882, 86.32497, 
+    90.28218, 94.41819, 98.74107}; // Adjust the bin edges as needed
   int nxbins = sizeof(xbins) / sizeof(xbins[0]) - 1;
 
   TDirectory *curdir = gDirectory;
@@ -60,6 +70,9 @@ void StrangeJet::Loop()
   TH1D *h_dus2 = new TH1D("h_dus2",";PtCand, energy;N", nxbins, xbins);
 
   TH1D *h_dusall = new TH1D("h_dusall",";PtCand, all S,U,D jets;N", nxbins, xbins);
+  TH1D *h_sall = new TH1D("h_sall",";PtCand, all S jets;N", nxbins, xbins);
+  TH1D *h_uall = new TH1D("h_uall",";PtCand, all U jets;N", nxbins, xbins);
+  TH1D *h_dall = new TH1D("h_dall",";PtCand, all D jets;N", nxbins, xbins);
 
   TH1D *h_d3 = new TH1D("h_d3",";PtCand, ratio nh;N", nxbins, xbins);
   TH1D *h_u3 = new TH1D("h_u3",";PtCand, ratio nh;N", nxbins, xbins);
@@ -82,14 +95,14 @@ void StrangeJet::Loop()
   TH1D *h_k0l = new TH1D("h_k0l",";PtCand, ratio K^0_L;N", nxbins, xbins);
   TH1D *h_k0s = new TH1D("h_k0s",";PtCand, ratio K^0_S;N", nxbins, xbins);
   TH1D *h_kp = new TH1D("h_kp",";PtCand, ratio K^+;N", nxbins, xbins);
-  TH1D *h_k0 = new TH1D("h_kp",";PtCand, ratio K^0;N", nxbins, xbins);
+  TH1D *h_k0 = new TH1D("h_k0",";PtCand, ratio K^0;N", nxbins, xbins);
   TH1D *h_lambda = new TH1D("h_lambda",";PtCand, ratio Lambda;N", nxbins, xbins);
   TH1D *h_sigma0 = new TH1D("h_sigma0",";PtCand, ratio Sigma^0;N", nxbins, xbins);
 
   TH1D *h_k0l2 = new TH1D("h_k0l2",";PtCand, ratio K^0_L;N", nxbins, xbins);
   TH1D *h_k0s2 = new TH1D("h_k0s2",";PtCand, ratio K^0_S;N", nxbins, xbins);
   TH1D *h_kp2 = new TH1D("h_kp2",";PtCand, ratio K^+;N", nxbins, xbins);
-  TH1D *h_k02 = new TH1D("h_kp2",";PtCand, ratio K^0;N", nxbins, xbins);
+  TH1D *h_k02 = new TH1D("h_k02",";PtCand, ratio K^0;N", nxbins, xbins);
   TH1D *h_lambda2 = new TH1D("h_lambda2",";PtCand, ratio Lambda;N", nxbins, xbins);
   TH1D *h_sigma02 = new TH1D("h_sigma02",";PtCand, ratio Sigma^0;N", nxbins, xbins);
   
@@ -129,28 +142,40 @@ void StrangeJet::Loop()
 	bool isSjet = (abs(GenJet_partonFlavour[i]) == 3);
 	bool isDjet = (abs(GenJet_partonFlavour[i]) == 1);
 	bool isUjet = (abs(GenJet_partonFlavour[i]) == 2 );
-  bool isPhoton = (GenPartCand_pdgId[i] == 22);
-  bool isKaonp = (GenPartCand_pdgId[i] == 321);
-  bool isKaon0 = (GenPartCand_pdgId[i] == 311);
-  bool isKaonL = (GenPartCand_pdgId[i] == 130);
-  bool isKaonS = (GenPartCand_pdgId[i] == 310);
-  bool isLambda = (GenPartCand_pdgId[i] == 3122);
-  bool isSigma = (GenPartCand_pdgId[i] == 3212);
+  bool isPhoton = (GenPartCand_pdgId[iCand] == 22);
+  bool isKaonp = (abs(GenPartCand_pdgId[iCand]) == 321);
+  bool isKaon0 = (GenPartCand_pdgId[iCand] == 311);
+  bool isKaonL = (GenPartCand_pdgId[iCand] == 130);
+  bool isKaonS = (GenPartCand_pdgId[iCand] == 310);
+  bool isLambda = (GenPartCand_pdgId[iCand] == 3122);
+  bool isSigma = (GenPartCand_pdgId[iCand] == 3212);
 
 
 	if (i == iJet) {
 	  // cout << "Cand" << iCand << ":" << GenPartCand_pt[iCand] << ", " ;
 	  double pt = GenJet_pt[iJet];
     double p2 = sqrt(pow(GenPartCand_pt[iCand],2)+pow(GenPartCand_mass[iCand],2));
-	  if (fabs(GenJet_eta[iJet]) < 1.3 && pt > 60 && pt < 100) {
+	  if (fabs(GenJet_eta[iJet]) < 1.3 && pt > 60 && pt < 140) {
 
 // All DUS jets
       if (isSjet || isUjet || isDjet) {
             h_dusall->Fill(GenPartCand_pt[iCand]);
       }
 
+      if (isSjet) {
+        h_sall->Fill(GenPartCand_pt[iCand]);
+      }
+
+      if (isUjet) {
+        h_uall->Fill(GenPartCand_pt[iCand]);
+      }
+
+       if (isDjet) {
+        h_dall->Fill(GenPartCand_pt[iCand]);
+      }
+
 // Neutral hadrons
-      if (GenPartCand_charge[iCand] == 0){
+      if (GenPartCand_charge[iCand] == 0 && !isPhoton){
 
         if (isSjet || isUjet || isDjet) {
           h_dus3->Fill(GenPartCand_pt[iCand]);
@@ -248,7 +273,7 @@ void StrangeJet::Loop()
 
       }
 
-/* // Neutral hadrons and photons
+ // Neutral hadrons and photons
       if (GenPartCand_charge[iCand] == 0 && isPhoton){
 
         if (isSjet || isUjet || isDjet) {
@@ -258,16 +283,88 @@ void StrangeJet::Loop()
         if (isSjet) {
           h_s5->Fill(GenPartCand_pt[iCand]);
           h_s6->Fill(p2);
+
+          if (isKaonL) {
+            h_k0l->Fill(GenPartCand_pt[iCand]);
+          }
+
+          if (isKaonS) {
+            h_k0s->Fill(GenPartCand_pt[iCand]);
+          }
+
+          if (isKaonp) {
+            h_kp->Fill(GenPartCand_pt[iCand]);
+          }
+
+          if (isKaon0) {
+            h_k0->Fill(GenPartCand_pt[iCand]);
+          }
+
+          if (isSigma) {
+            h_sigma0->Fill(GenPartCand_pt[iCand]); 
+          }
+
+          if (isLambda) {
+            h_lambda->Fill(GenPartCand_pt[iCand]); 
+          }
         }
         if (isDjet) {
           h_d5->Fill(GenPartCand_pt[iCand]);
           h_d6->Fill(p2);
+
+          if (isKaonL) {
+            h_k0l2->Fill(GenPartCand_pt[iCand]);
+          }
+
+          if (isKaonS) {
+            h_k0s2->Fill(GenPartCand_pt[iCand]);
+          }
+
+          if (isKaonp) {
+            h_kp2->Fill(GenPartCand_pt[iCand]);
+          }
+
+          if (isKaon0) {
+            h_k02->Fill(GenPartCand_pt[iCand]);
+          }
+
+          if (isSigma) {
+            h_sigma02->Fill(GenPartCand_pt[iCand]); 
+          }
+
+          if (isLambda) {
+            h_lambda2->Fill(GenPartCand_pt[iCand]); 
+          }
         }
         if (isUjet) {
           h_u5->Fill(GenPartCand_pt[iCand]);
           h_u6->Fill(p2);
+
+          if (isKaonL) {
+            h_k0l2->Fill(GenPartCand_pt[iCand]);
+          }
+
+          if (isKaonS) {
+            h_k0s2->Fill(GenPartCand_pt[iCand]);
+          }
+
+          if (isKaonp) {
+            h_kp2->Fill(GenPartCand_pt[iCand]);
+          }
+
+          if (isKaon0) {
+            h_k02->Fill(GenPartCand_pt[iCand]);
+          }
+
+          if (isSigma) {
+            h_sigma02->Fill(GenPartCand_pt[iCand]); 
+          }
+
+          if (isLambda) {
+            h_lambda2->Fill(GenPartCand_pt[iCand]); 
+          }
         }
-      }*/
+      }
 
 // Charged hadrons
       if (GenPartCand_charge[iCand] != 0){
